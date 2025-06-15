ad01f2907d4fac107741a6f97d48312ba747348d
Ohhyeun Kwon <35871896+kdhgus92@users.noreply.github.com>
2025-06-15 23:28:04 +0900

    chatRoom

---
+++ src/app/(afterLogin)/[username]/status/[id]/photo/[photoId]/page.tsx
@@ -0,0 +1,10 @@
+import Home from "@/app/(afterLogin)/home/page";
+
+type Props = {
+  params: { username: string; id: string; photoId: string };
+};
+export default async function Page({ params }: Props) {
+  const { username, id, photoId } = await params;
+
+  return <Home />;
+}
